139d5718f49b3b6ad66a89aa17fdb2cc251542d0
sjoerdjansz <sjoerdjansz@gmail.com>
2025-04-30 19:21:38 +0200

    opdracht 2 deels gemaakt en meer styling plus optimalisatie

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css';
 
+import {totalProductsSold, totalProductsInStock, productsToBeSold} from "./helpers/productsSold.js";
+import {createNameString, getScreenSizes, priceFormatter} from "./helpers/productPlacement.js";
+import {inventory, bestSellingTv} from "./constants/inventory";
+
 function App() {
     return (
         <main>
@@ -10,29 +14,27 @@ function App() {
                 <div className="product-stats-container">
                     <article className="products-sold">
                         <p>Aantal verkochte producten</p>
-                        <p>101</p>
+                        <p>{totalProductsSold(inventory)}</p>
                     </article>
                     <article className="products-stock">
-                        <p>Aantal verkochte producten</p>
-                        <p>102</p>
+                        <p>Aantal ingekochte producten</p>
+                        <p>{totalProductsInStock(inventory)}</p>
                     </article>
                     <article className="products-to-sell">
-                        <p>Aantal verkochte producten</p>
-                        <p>103</p>
+                        <p>Aantal te verkopen producten</p>
+                        <p>{productsToBeSold(totalProductsInStock(inventory), totalProductsSold(inventory))}</p>
                     </article>
                 </div>
             </section>
-
             <section className="overview-container">
                 <h2>Best verkochte TV</h2>
                 <article className="popular-product-container">
-                    <div className="img-container"><img src="/assets/tech_it_easy.png" alt="test-image"/>
+                    <div className="img-container"><img src={bestSellingTv.sourceImg} alt="test-image"/>
                     </div>
                     <div className="product-info-container">
-                        <h3 className="title">Samsung UHD 55AU7040 - Crystal</h3>
-                        <p className="price">€549,-</p>
-                        <p className="sizes">43 inch (109 cm) | 50 inch (127 cm) | 55 inch (140 cm) | 65 inch
-                            (165 cm)</p>
+                        <h3 className="title">{createNameString(bestSellingTv)}</h3>
+                        <p className="price">{priceFormatter(bestSellingTv)}</p>
+                        <p className="sizes">{getScreenSizes(bestSellingTv)}</p>
                         <p className="options">[check-icon] wifi [not-icon] speech [check-icon] hdr
                             [check-icon] bluetooth [not-icon] ambilight
                         </p>
---
+++ src/helpers/productPlacement.js
@@ -0,0 +1,32 @@
+import {bestSellingTv} from '../constants/inventory.js';
+
+export function createNameString(arr) {
+    if (!arr.brand || !arr.name || !arr.type) {
+        return 'Unknown TV details. Contact support.';
+    }
+    return `${arr.brand} ${arr.type} ${arr.name}`;
+}
+
+export function priceFormatter(arr) {
+    if (!arr.price) {
+        return 'Price unknown. Contact support.';
+    }
+    return `€${arr.price},–`;
+}
+
+export function getScreenSizes(arr) {
+    let sizes = arr.availableSizes;
+    let result = "";
+    for (let i = 0; i < sizes.length; i++) {
+        if (i === sizes.length - 1) {
+            result += `${sizes[i]} inches (${sizes[i] * 2.54.toFixed()} cm)`;
+        } else {
+            result += `${sizes[i]} inches (${sizes[i] * 2.54.toFixed()} cm) | `;
+        }
+    }
+    return result;
+}
+
+console.log(createNameString(bestSellingTv));
+console.log(priceFormatter(bestSellingTv));
+console.log(getScreenSizes(bestSellingTv));
