1ae5ec82195270e8283d508e1dc749ab7942d43c
sjoerdjansz <sjoerdjansz@gmail.com>
2025-05-19 22:13:49 +0200

    alle opdrachten afgemaakt behalve de bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import './App.css';
+import "./App.css";
 
 // helper functions
-import {totalProductsSold, totalProductsInStock, productsToBeSold} from "./helpers/productsSold.js";
+import {
+    totalProductsSold,
+    totalProductsInStock,
+    productsToBeSold,
+} from "./helpers/productsSold.js";
 import {
     createNameString,
     getScreenSizes,
     priceFormatter,
 } from "./helpers/productPlacement.js";
 
-
 // import logger functies voor console.logs voor exercise 1 en 2
 import {logsFromExerciseOne} from "./helpers/logger.js";
-import showOutcomeInConsole from './constants/oefenbestand.js';
+import showOutcomeInConsole from "./constants/oefenbestand.js";
 
 // data
 import {inventory, bestSellingTv} from "./constants/inventory";
 
 // icons
-import check from './assets/check.png';
-import minus from './assets/minus.png';
+import check from "./assets/check.png";
+import minus from "./assets/minus.png";
 
 function App() {
-    function handleClick(e) {
-        console.log(e.target.innerText);
+    function sortTvSold(data) {
+        return data.sort((a, b) => {
+            return b.sold - a.sold;
+        });
+    }
+
+    function sortTvPrice(data) {
+        return data.sort((a, b) => {
+            return a.price - b.price;
+        });
+    }
+
+    function sortTvSport(data) {
+        return data.sort((a, b) => {
+            return b.refreshRate - a.refreshRate;
+        });
     }
 
     // loggers voor exercises 1 en 2
@@ -46,25 +63,46 @@ function App() {
                     </article>
                     <article className="products-to-sell">
                         <p>Aantal te verkopen producten</p>
-                        <p>{productsToBeSold(totalProductsInStock(inventory), totalProductsSold(inventory))}</p>
+                        <p>
+                            {productsToBeSold(
+                                totalProductsInStock(inventory),
+                                totalProductsSold(inventory)
+                            )}
+                        </p>
                     </article>
                 </div>
             </section>
             <section className="overview-container">
                 <h2>Best verkochte TV</h2>
                 <article className="popular-product-container">
-                    <div className="img-container"><img src={bestSellingTv.sourceImg} alt="tv-image"/>
+                    <div className="img-container">
+                        <img src={bestSellingTv.sourceImg} alt="tv-image"/>
                     </div>
                     <div className="product-info-container">
                         <h3 className="title">{createNameString(bestSellingTv)}</h3>
                         <p className="price">{priceFormatter(bestSellingTv)}</p>
                         <p className="sizes">{getScreenSizes(bestSellingTv)}</p>
                         <ul className="options">
-                            <li><img src={check} alt="check-icon"/>wifi</li>
-                            <li><img src={minus} alt="minus-icon"/>speech</li>
-                            <li><img src={check} alt="check-icon"/>hdr</li>
-                            <li><img src={check} alt="check-icon"/>bluetooth</li>
-                            <li><img src={minus} alt="minus-icon"/>ambilight</li>
+                            <li>
+                                <img src={check} alt="check-icon"/>
+                                wifi
+                            </li>
+                            <li>
+                                <img src={minus} alt="minus-icon"/>
+                                speech
+                            </li>
+                            <li>
+                                <img src={check} alt="check-icon"/>
+                                hdr
+                            </li>
+                            <li>
+                                <img src={check} alt="check-icon"/>
+                                bluetooth
+                            </li>
+                            <li>
+                                <img src={minus} alt="minus-icon"/>
+                                ambilight
+                            </li>
                         </ul>
                     </div>
                 </article>
@@ -72,10 +110,26 @@ function App() {
             <section className="overview-container">
                 <h2>Alle Televisies</h2>
                 <div className="filter-container">
-                    <button onClick={handleClick} className="filter-btn" type="button">Meest verkocht
+                    <button
+                        onClick={() => console.log(sortTvSold(inventory))}
+                        className="filter-btn"
+                        type="button"
+                    >
+                        Meest verkocht
                     </button>
-                    <button onClick={handleClick} className="filter-btn" type="button">Goedkoopste</button>
-                    <button onClick={handleClick} className="filter-btn" type="button">Geschikt voor sport
+                    <button
+                        onClick={() => console.log(sortTvPrice(inventory))}
+                        className="filter-btn"
+                        type="button"
+                    >
+                        Goedkoopste
+                    </button>
+                    <button
+                        onClick={() => console.log(sortTvSport(inventory))}
+                        className="filter-btn"
+                        type="button"
+                    >
+                        Geschikt voor sport
                     </button>
                 </div>
                 <div className="filter-result-container">
@@ -83,31 +137,36 @@ function App() {
                     {/*    return <li key={i}>{value.type}</li>;*/}
                     {/*})}*/}
                     {inventory.map((value, i) => {
-                        return <article key={i} className="popular-product-container">
-                            <div className="img-container"><img src={value.sourceImg}
-                                                                alt="tv-image"/>
-                            </div>
-                            <div className="product-info-container">
-                                <h3 className="title">{createNameString(value)}</h3>
-                                <p className="price">{priceFormatter(value)}</p>
-                                <p className="sizes">{getScreenSizes(value)}</p>
-                                <ul className="options">
-                                    {value.options.map((option, i) => {
-                                        return option.applicable ? <li key={i}>
-                                                <img src={check}
-                                                     alt="check-icon"/>{option.name}</li> :
-                                            <li key={i}>
-                                                <img src={minus}
-                                                     alt="minus-icon"/>{option.name}</li>;
-                                    })}
-                                </ul>
-                            </div>
-                        </article>;
+                        return (
+                            <article key={i} className="popular-product-container">
+                                <div className="img-container">
+                                    <img src={value.sourceImg} alt="tv-image"/>
+                                </div>
+                                <div className="product-info-container">
+                                    <h3 className="title">{createNameString(value)}</h3>
+                                    <p className="price">{priceFormatter(value)}</p>
+                                    <p className="sizes">{getScreenSizes(value)}</p>
+                                    <ul className="options">
+                                        {value.options.map((option, i) => {
+                                            return option.applicable ? (
+                                                <li key={i}>
+                                                    <img src={check} alt="check-icon"/>
+                                                    {option.name}
+                                                </li>) : (
+                                                <li key={i}>
+                                                    <img src={minus} alt="minus-icon"/>
+                                                    {option.name}
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            </article>
+                        );
                     })}
                 </div>
             </section>
         </main>
-
     );
 }
 
